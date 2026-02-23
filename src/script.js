adcd568a125764b3cb50549c3c85c1941184aa01
md-kudrot-592 <kk.mdkudrot@gmail.com>
2026-02-23 13:30:58 +0600

    Refactor job application tracker: update section IDs, enhance button functionality, and improve event handling for job interactions

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const mainSection = document.getElementById("mainSection")
+const mainCartSection = document.getElementById("mainCartSection")
 let total = document.getElementById("total")
 let interviewCount = document.getElementById("interviewCount")
 let rejectedCount = document.getElementById("rejectedCount")
@@ -13,7 +13,7 @@ const rejectedArr = [];
 
 
 function countLen() {
-    total.innerText = mainSection.children.length;
+    total.innerText = mainCartSection.children.length;
     interviewCount.innerText = interviewArr.length;
     rejectedCount.innerText = rejectedArr.length;
 }
@@ -21,17 +21,58 @@ function countLen() {
 countLen()
 
 function toggleFunc(id) {
-    allBtn.classList.remove('bg-[#3B82F6]' ,'text-white' )
-    interviewBtn.classList.remove('bg-[#3B82F6]' ,'text-white' )
-    rejectedBtn.classList.remove('bg-[#3B82F6]' ,'text-white' )
-    
+    allBtn.classList.remove('bg-[#3B82F6]', 'text-white')
+    interviewBtn.classList.remove('bg-[#3B82F6]', 'text-white')
+    rejectedBtn.classList.remove('bg-[#3B82F6]', 'text-white')
+
     allBtn.classList.add('bg-white', 'text-[#64748B]')
     interviewBtn.classList.add('bg-white', 'text-[#64748B]')
     rejectedBtn.classList.add('bg-white', 'text-[#64748B]')
-    
+
     const selectedId = document.getElementById(id)
     console.log(selectedId)
     selectedId.classList.remove('bg-white', 'text-[#64748B]')
-    selectedId.classList.add('bg-[#3B82F6]' ,'text-white' )
-    
+    selectedId.classList.add('bg-[#3B82F6]', 'text-white')
+
 }
+
+mainCartSection.addEventListener("click", (event) => {
+    console.log(event.target)
+    if (event.target.classList.contains('interviewButton')) {
+
+
+        // console.log(event.target.parentElement.parentElement)
+        const cart = event.target.parentElement.parentElement
+        // console.log(cart)
+        const companyName = cart.querySelector(".companyName").innerText
+        const position = cart.querySelector(".position").innerText
+        const appliedButton = cart.querySelector(".applied-button").innerText
+        const description = cart.querySelector(".description").innerText
+        const interviewButton = cart.querySelector(".interviewButton").innerText
+        const rejectedButton = cart.querySelector(".rejectedButton").innerText
+
+        const cartData = {
+            companyName,
+            position,
+            appliedButton: "Appiled",
+            description,
+            interviewButton,
+            rejectedButton
+        }
+
+        console.log(cartData)
+        // interviewArr.push(cartData)
+        // console.log(interviewArr.companyName === cartData.companyName)
+        const cartExist = interviewArr.find(item => item.companyName == cartData.companyName)
+        // console.log(cartExist)
+        if (!cartExist) {
+            interviewArr.push(cartData)
+
+        }
+        countLen()
+    }
+
+
+
+    // console.log(companyName)
+})
